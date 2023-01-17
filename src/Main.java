773ce6f8df041c8bed7d0d4e73b049e5955d644a
vascojonas <vascojonas.j@gmail.com>
2023-01-17 13:03:30 +0200

    Encapsulamento

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Curso curso1 = new Curso();
+		
+		curso1.setTitulo("Curso de Java");
+		curso1.setDescricao("Descricao  curso java");
+		curso1.setCargaHoraria(8);
+		curso1.setTitulo("Curso de Java");
+
+		Curso curso2 = new Curso();
+		
+		curso2.setTitulo("Curso de JS");
+		curso2.setDescricao("Descricao  curso Js");
+		curso2.setCargaHoraria(4);
+		
+		
+		Mentoria mentoria = new Mentoria();
+		
+		mentoria.setTitulo("Mentoria de Java");
+		mentoria.setDescricao("Descricao da mentoria de Java");
+		mentoria.setData(LocalDate.now());
+		
+		System.out.println(curso1);
+		System.out.println(curso2);
+		System.out.println(mentoria);
+	}
+
+}
